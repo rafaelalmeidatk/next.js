7f67896a0fa9d00ba3c7e58368995b5a24469a8d
chislee0708 <chislee0708@gmail.com>
2020-04-28 19:31:32 -0400

    Added GET, PUT, DELETE by id

---
+++ examples/with-mongodb/pages/api/pets/[id].js
@@ -0,0 +1,59 @@
+import dbConnect from '../../../utils/dbConnect';
+import Pet from '../../../models/Pet';
+
+dbConnect();
+
+export default async (req, res) => {
+    const { 
+        query: { id },
+        method
+     } = req;
+
+    switch(method){
+        case 'GET':
+            try{
+                const pet = await Pet.findById(id); 
+
+                if (!pet){
+                    return res.status(400).json({ success: false});
+                }
+
+                res.status(200).json({ success: true, data: pet })
+            } catch (error){
+                res.status(400).json({ success: false });
+            }
+            break;
+        case 'PUT':
+            try {
+                const pet = await Pet.findByIdAndUpdate(id, req.body,{
+                    new: true,
+                    runValidators: true
+                });
+
+                if(!pet){
+                    return res.status(400).json({ success: false});
+                }
+
+                res.status(200).json({ success: true, data: pet});
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+        case 'DELETE':
+            try {
+                const deletedPet = await Pet.deleteOne({ _id: id });
+
+                if (!deletedPet) {
+                    return res.status(400).json({ success: false })
+                }
+
+                res.status(200).json({ success: true, data: {} });
+            } catch (error) {
+                res.status(400).json({ success: false});
+            }
+            break;
+        default:
+            res.status(400).json({ success: false });
+            break;
+    }
+}
