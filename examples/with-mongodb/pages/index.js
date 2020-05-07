2a1e1e860a8656d211c9ba941adfddd3b71c02eb
MichelleLucero <michelle.lucero07@myhunter.cuny.edu>
2020-05-06 20:17:31 -0400

    displays the homepage, which consists of pet info cards

--- examples/with-mongodb/pages/index.js
+++ examples/with-mongodb/pages/index.js
@@ -1,26 +1,53 @@
-import Head from 'next/head'
-// import pets from '../seed/Pet-seeder'
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch' //alternative dbconnect into getStaticProps??
+import { Button, Card, Image } from 'semantic-ui-react';
 
-export default function Home({pets}) {
+/* Displays the homepage, which consists of pet cards*/
+const Index = ({ pets }) => {
   return (
-    <div>
-      <h1>Pet Care</h1>
-      <div className="pet-card"> 
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Charlie_April_2020.jpg/588px-Charlie_April_2020.jpg"></img>
-        <div>
-          <h3>name: Charlie</h3>
-          <p>owner name: Michelle</p>
-        </div>  
-        <p>species: Dog</p>
-        <p>age: 9</p>
-        <p>poddy_trained: yes</p>
-        <p>diet: "Sweet Potato", "Rabbit & Potato dry food", "Melon", "Carrots",</p>
-        <p>likes: "Cuddling in the morning", "New places to pee", "Going to pet stores", "Sunbathing"</p>
-        <p>dislikes: "Not being greeted", "Watching others eat", "Oral medication", "Showers", "Brushing his teeth","Cuddling at night"</p>
-        
+    <div className="notes-container">
+      <h1>Pets</h1>
+      <div className="grid wrapper">
+        {pets.map( pet => { return (
+          <div key={pet._id}>
+            {/* Instances of pet card */}
+            <Card>
+              <Image src={pet.image_url} wrapped ui={false} />
+              <Card.Content>
+                <Card.Header>{pet.name}</Card.Header>
+                <Card.Meta>
+                  <span className='owner'>Owner: {pet.owner_name}</span>
+                </Card.Meta>
+                <Card.Description>
+                  {/* May be a good idea to display all info on pets*/}
+                  {pet.likes.map(s => <span key={s}>{s} </span>)}
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <div>
+                  <Link href={`/${pet._id}/edit`}>
+                    <Button basic color='green'>Edit</Button>
+                  </Link>
+                  <Link href={`/${pet._id}`}>
+                    <Button basic color='red'>Delete</Button>
+                  </Link>
+                </div>
+
+              </Card.Content>
+            </Card>
+          </div>
+        )})}
       </div>
-
-
-    </div>  
+    </div>
   )
 }
+
+/* Retrieves pet(s) data from mongodb database */
+Index.getInitialProps = async () => {
+  const res = await fetch('http://localhost:3000/api/pets');
+  const { data } = await res.json();
+
+  return { pets: data }
+}
+
+export default Index;
