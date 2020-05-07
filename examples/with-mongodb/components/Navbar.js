59ffc4f8471af0b446f1ae07258df2e93cda2bad
chislee0708 <chislee0708@gmail.com>
2020-05-07 16:14:22 -0400

    Edited style.css and navigation bar.

--- examples/with-mongodb/components/Navbar.js
+++ examples/with-mongodb/components/Navbar.js
@@ -1,17 +1,18 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-const Navbar = () => (
-    <div className="navbar">
-        <Link href="/">
-            <a className="brand">
-                <h3>Pet Care</h3>
-            </a>
-        </Link>
-        <Link href="/new">
-            <a className="add-pet">
-                <h3>Add Pet</h3>
-            </a>
-        </Link>
+const Navbar = () =>(
+    <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
+    <div className="container">
+      <a className="navbar-brand" href="#"><h3>Pet Care</h3></a>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link href="/new"><a className="nav-link"><h3>Add Pet</h3></a></Link>
+          </li>
+        </ul>
+      </div>
     </div>
-)
+  </nav>
+);
+
 export default Navbar;
